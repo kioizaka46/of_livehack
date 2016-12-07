900bd8589eac0c7f6aa4524c1a757499fd851f83
西本　高志 <takasnis@MBA-11JAL-068.local>
2016-12-07 13:17:32 +0900

    modified:   src/ofApp.cpp 	modified:   src/ofApp.h

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,108 +1,88 @@
 #include "ofApp.h"
 
+
 //--------------------------------------------------------------
-void ofApp::setup(){
-    music.load("keyaki_silent_majority.mp3");
-    music.setMultiPlay(true);
+void ofApp::setup() {
+    ofSetVerticalSync(true);
+    ofBackgroundHex(0xfdefc2);
+    ofSetLogLevel(OF_LOG_NOTICE);
     
-    font.loadFont("SmartFontUI.ttf", 100, true, true);
-    std::string file = "sync_lylic_silent_majority.json";
+    bMouseForce = false;
     
-    drop_point_x = 100.0;
-    drop_point_y = 350.0;
-    start_point_x = 1024.0;
-    start_point_y = 350.0;
+    box2d.init();
+    box2d.setGravity(0, 30);
+    box2d.createGround();
+    box2d.setFPS(100.0);
+    box2d.registerGrabbing();
+    box2d.createBounds(0, 0, ofGetWidth(), ofGetHeight());
+
+}
+//--------------------------------------------------------------
+void ofApp::update() {
     
-    bool parsingSuccessful = sync_lylic_json.open(file);
+    box2d.update();
     
-    if (parsingSuccessful){
-        for(int i=0; i < sync_lylic_json["lines"].size(); i++){
-            for(int j=0; j < sync_lylic_json["lines"][i]["words"].size(); j++){
-                TextSymbol ts;
-                ts.size = 10;
-                ts.start_time = sync_lylic_json["lines"][i]["words"][j]["start"].asDouble();
-                ts.end_time = sync_lylic_json["lines"][i]["words"][j]["end"].asDouble();
-                ts.text = sync_lylic_json["lines"][i]["words"][j]["string"].asString();
-                ts.pos_x = start_point_x;
-                ts.pos_y = start_point_y;
-                ts.is_draw = true;
-                text_symbols.push_back(ts);
-            }
+    if(bMouseForce) {
+        float strength = 1.0f;
+        float damping  = 1.0f;
+        for(int i=0; i<customParticles.size(); i++) {
+            customParticles[i].get()->addAttractionPoint(mouseX, mouseY, strength);
+            customParticles[i].get()->setDamping(damping, damping);
         }
-    }else{
-        ofLogError("ofApp::setup")  << "Failed to parse JSON" << endl;
     }
-    music.play();
-}
-//--------------------------------------------------------------
-void ofApp::update(){
-    ofSoundUpdate();
 }
 
+
 //--------------------------------------------------------------
-void ofApp::draw(){
-    ofBackground(155,155,155);
-    ofSetHexColor(0x000000);
+void ofApp::draw() {
+    for(int i=0; i<customParticles.size(); i++) {
+        customParticles[i].get()->draw();
+    }
+    ofFill();
     
-    float music_pos = music.getPositionMS();
-    for(int i = 0; i< text_symbols.size(); i++){
-        text_symbols[i].pos_x = text_symbols[i].end_time - music_pos;
-        font.drawString(text_symbols[i].text, text_symbols[i].pos_x, text_symbols[i].pos_y);
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key) {
+    
+    if(key == 'c') {
+        customParticles.push_back(shared_ptr<CustomParticle>(new CustomParticle));
+        CustomParticle * p = customParticles.back().get();
+        float r = ofRandom(50, 51);
+        p->setPhysics(0.1, 0.9, 0.5);
+        p->setup(box2d.getWorld(), mouseX, mouseY, r);
+        //p->color.r = ofRandom(20, 100);
+        //p->color.g = 0;
+        //p->color.b = ofRandom(150, 255);
     }
-    font.drawString("|",drop_point_x,drop_point_y);
 }
 
 //--------------------------------------------------------------
-void ofApp::keyPressed(int key){
-
+void ofApp::keyReleased(int key) {
+    
 }
 
 //--------------------------------------------------------------
-void ofApp::keyReleased(int key){
-
+void ofApp::mouseMoved(int x, int y) {
+    
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
+void ofApp::mouseDragged(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
+void ofApp::mousePressed(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-
+void ofApp::mouseReleased(int x, int y, int button) {
+    
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
+void ofApp::resized(int w, int h){
+    
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -1,53 +1,57 @@
 #pragma once
-
 #include "ofMain.h"
-#include "ofxJSON.h"
-#include "ofxTrueTypeFontUC.h"
+#include "ofxBox2d.h"
 
-struct TextSymbol {
-    std::string text;
-    double start_time;
-    double end_time;
-    int size;
-    double pos_x;
-    double pos_y;
-    bool is_draw;
+// ------------------------------------------------- a simple extended box2d circle
+class CustomParticle : public ofxBox2dCircle {
+    
+public:
+    CustomParticle() {
+    }
+    ofColor color;
+    ofImage img;
+    ofTrueTypeFont font;
+    
+    void draw() {
+        float radius = getRadius();
+        
+        glPushMatrix();
+        glTranslatef(getPosition().x, getPosition().y, 0);
+        
+        ofSetColor(color.r, color.g, color.b);
+        ofFill();
+        //ofDrawCircle(0, 0, radius);
+        img.load("3.png");
+        img.draw(0,0, radius);
+        //font.loadFont("Narrow.ttf", 42);
+        //font.drawString("Love", 0, 0);
+        
+        glPopMatrix();
+    }
 };
-
-class ofApp : public ofBaseApp{
-
-	public:
-		void setup();
-		void update();
-		void draw();
-
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
+// -------------------------------------------------
+class ofApp : public ofBaseApp {
     
-        // sounds
-        ofSoundPlayer music;
-        float synthPosition;
+public:
     
-        // io
-        ofxJSONElement sync_lylic_json;
-        ofxTrueTypeFontUC font;
+    void setup();
+    void update();
+    void draw();
     
-        // lyrics
-        std::vector<TextSymbol> text_symbols;
-        string view_lylic;
+    void keyPressed(int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y);
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void resized(int w, int h);
     
-        // animation
-        double drop_point_x;
-        double drop_point_y;
-        double start_point_x;
-        double start_point_y;
+    bool bMouseForce;
+    
+    ofxBox2d box2d;           // the box2d world
+    ofPolyline drawing;         // we draw with this first
+    ofxBox2dEdge edgeLine;        // the box2d edge/line shape (min 2 points)
+    vector <shared_ptr<ofxBox2dCircle> > circles;         // default box2d circles
+    vector <shared_ptr<ofxBox2dRect> >   boxes;           // default box2d rects
+    vector <shared_ptr<CustomParticle> > customParticles; // this is a custom particle the extends a cirlce
 };
